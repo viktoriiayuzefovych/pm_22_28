a238d79d9630788c997453bd25a1b75c5ec421e3
viktoriiayuzefovych <mariia-viktoriia.yuzefovych.pm.2023@lpnu.ua>
2024-11-07 11:08:29 +0200

    fixed server

--- app/js/script.js
+++ app/js/script.js
@@ -1,29 +1,42 @@
-document.querySelectorAll(".skill-item").forEach((skill) => {
-    const progress = skill.querySelector(".progress");
-  
-    const width = progress.style.width; // Наприклад, "80%"
-  
-    progress.style.setProperty("--progress-width", width);
+//import { name } from "browser-sync";
+const openButtons = document.getElementsByClassName('open-info-btn');
+const infoBoxes = document.getElementsByClassName('open-info-box');
+const mainInfoBoxes = document.getElementsByClassName('main-info-box');
+const progressBars = document.getElementsByClassName('mn-progress-bar');
+
+Array.from(openButtons).forEach((openButton, index) => {
+  openButton.addEventListener('click', () => showOrHide(openButton, infoBoxes[index], mainInfoBoxes[index]));
 });
-  document.addEventListener("DOMContentLoaded", function () {
-    const skills = document.querySelectorAll(".skill-item");
+
+function setProgressWidth() {
+  document.querySelectorAll(".skill-item").forEach((skillBox) => {
+    const progress = skillBox.querySelector(".progress");
+    const width = progress.style.width; // Наприклад, "55%"
+    progress.style.setProperty("--progress-width", width); // Зберігаємо ширину у CSS-змінній
+  });
+}
+
+function animateSkillBoxVisibility() {
+  const skillBoxes = document.querySelectorAll(".skill-item");
   
-    skills.forEach((skill, index) => {
-      setTimeout(() => {
-        skill.classList.add("visible"); // Додаємо клас видимості
-      }, index * 300); // Затримка для кожного елемента
-    });
+  skillBoxes.forEach((skillBox, index) => {
+    setTimeout(() => {
+      skillBox.classList.add("visible"); // Додаємо клас видимості
+    }, index * 300); // Затримка для кожного елемента
   });
-  document.addEventListener("DOMContentLoaded", function () {
-    const progressBars = document.querySelectorAll(".progress");
-    progressBars.forEach((progressBar) => {
-      const width = progressBar.style.width; // Отримуємо задану ширину
-      progressBar.style.width = "0"; // Задаємо початкову ширину 0
-      setTimeout(() => {
-        progressBar.style.width = width; // Відновлюємо ширину для анімації
-      }, 100); // Затримка для початку анімації
-    });
+}
+function animateProgressBars() {
+  const progressBars = document.querySelectorAll(".progress");
+  
+  progressBars.forEach((progressBar) => {
+    const width = progressBar.style.width; // Отримуємо початкову ширину
+    progressBar.style.width = "0"; // Спочатку встановлюємо ширину 0
+    setTimeout(() => {
+      progressBar.style.width = width; // Відновлюємо початкову ширину для анімації
+    }, 100); // Затримка перед початком анімації
   });
+}
+
   
   const btnRoll = document.getElementsByClassName("roll");
   const blockRoll = document.getElementsByClassName("roll-block");
@@ -38,42 +51,46 @@ document.querySelectorAll(".skill-item").forEach((skill) => {
     }
   }
 
+
   document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://localhost:5000/data')
+    fetch('http://localhost:8080/data')
         .then(response => response.json())
         .then(data => {
           console.log(data);
-           // Робота з секцією 'skills'
-           /*const skillsContainer = document.getElementById('skills');
+
+          const profileContainer = document.getElementById('profile');
+          const profileHTML = data.profile.map(proff => `
+              <span style="font-weight: 800; font-style: italic; font-size: 1.4rem; letter-spacing: 0.06rem;color: #425055;">${proff.name}</span> <span style="font-weight:300; font-size: 1.4rem; letter-spacing: 0.06rem; color: #425055;">${proff.text}</span>                  
+                        
+          `).join('');
+          profileContainer.innerHTML = profileHTML;
+          console.log(profileHTML);
+
+           const skillsContainer = document.getElementById('skills');
            const skillsHTML = data.skills.map(skill => `
-               <div class="skill-item">
-                 <div class="d-flex">
-                   <span class="marker"></span>
-                   <p class="skill">${skill.name}</p>
-                 </div>
-                 <div class="progress-bar">
-                   <div class="progress" style="width: ${skill.progress}"></div>
-                 </div>
-               </div>
+            <div class="skill-item">
+                                    <span>${skill.name}</span>
+                                <div class="progress-bar">
+                                    <div class="progress" style="width: ${skill.progress}"></div>
+                                </div>
+                            </div>
            `).join('');
-           skillsContainer.innerHTML = skillsHTML;*/
+           skillsContainer.innerHTML = skillsHTML;
 
            const referencesContainer = document.getElementById('referen');
-           const referencesHTML = data.references.map(ref => `
-                 <div class="first-comp" style="background-color: #425055;">
-                            <h3 style="color: #fff; font-size: 1.3rem; letter-spacing: 0.06rem; text-transform: uppercase;">${ref.name}</h3>
+           const referencesHTML = data.referen.map(ref => `
+            <div class="first-comp">
+                            <h3> ${ref.name}</h3>
                             <p>${ref.address}</p>
-                            <span>${ref.phone}</span><a href="tel:+1-970-533-3393">+1-970-533-3393</a> 
-                            <span>${ref.email}</span><a href="https://www.yourwebsite.com">www.yourwebsite.com</a>
-                        </div>
-               </div>
+                            <span>Tel:</span><a href="tel:${ref.phone}">${ref.phone}</a> 
+                            <span>Email:</span><a href="https://${ref.email}">${ref.email}</a>
+            </div>
+                
            `).join('');
            referencesContainer.innerHTML = referencesHTML;
- 
-           // Після завантаження даних ініціюємо анімації
+
            setProgressWidth();
            animateSkillBoxVisibility();
            animateProgressBars();
-           animateShapes();
-   })
+    })
   });
--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const port = 5000;
+const port =  8080;
 
 app.use(cors());
 
 app.get('/data', (req, res) => {
     res.json({
-        /*skills:[
+        profile:[
+            {name: "Loran Ipsum",
+            text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+        ],
+        skills:[
             { name: "Adobe Photoshop", progress: "83.3%" },
             { name: "Adobe Illustrator", progress: "76%" },
             { name: "HTML-5/CSS-3", progress: "63.3%" },
             { name: "Microsoft Word", progress: "69%" },
             { name: "Microsoft Powerpoint", progress: "64%" }
-        ];*/
+        ],
         referen:[
-            {name: "Darwin Magana" },
-            { address: "2813 Shobe Lane Mancos, CO."},
-            { phone: "Tel:"},
-            { email:"Email:"}, 
-
-            {name: "Darwin Magana" },
-            { address: "2813 Shobe Lane Mancos, CO."},
-            { phone: "Tel:"},
-            { email:"Email:"}
+            { name: "Darwin Magana",
+                address: "2813 Shobe Lane Mancos, CO.",
+                phone: "+1-970-533-3393",
+                email:"www.yourwebsite.com"
+               }, 
+       
+               {name: "Darwin Magana",
+                address: "2813 Shobe Lane Mancos, CO.",
+                phone: "+1970-533-3393",
+                email:"www.yourwebsite.com"
+               }
         ],
 
     })
